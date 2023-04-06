b835cfcc9e734175493b3a682ee709187fe53241
Thomas Wray <twray95@gmail.com>
2023-04-06 17:40:14 -0600

    added reaction model and filled out other models

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,18 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({});
+const thoughtSchema = new Schema({
+  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
 
 const Thought = model("thought", thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,42 @@
 const { Schema, model } = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+require("mongoose-type-email");
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: mongoose.SchemaTypes.email,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 const User = model("user", userSchema);
 
