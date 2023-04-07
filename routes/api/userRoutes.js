45795c1b6ff97adbe5236e860094a7c2092ba793
Thomas Wray <twray95@gmail.com>
2023-04-06 21:26:26 -0600

    added friend routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,19 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  getFriends,
+  addFriend,
+  deleteFriend,
 } = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
 
 router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends")
+  .get(getFriends)
+  .post(addFriend)
+  .delete(deleteFriend);
 
 module.exports = router;
