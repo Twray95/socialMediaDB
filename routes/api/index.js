a9c001a8a0c5aafa7d432a9b973f60f52e1d74c3
Thomas Wray <twray95@gmail.com>
2023-04-06 19:32:49 -0600

    finished user routes, added modular friend and reaction routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const thoughtRoutes = require("./thoughtRoutes");
 const userRoutes = require("./userRoutes");
+const reactionRoutes = require("./reactionRoutes");
+const friendRoutes = require("./friendRoutes");
 
 router.use("/thoughts", thoughtRoutes);
 router.use("/users", userRoutes);
+router.use("/reactions", reactionRoutes);
+router.use("/friends", friendRoutes);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,10 +3,12 @@ const {
   getUsers,
   getOneUser,
   createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
 
-router.route("/:userId").get(getOneUser);
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
